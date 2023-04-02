a96e3c7adb890fc47ca51e33943b5248770a8a6a
krishna soni <soni01krishna@gmail.com>
2023-04-02 20:38:22 +0530

    footer  is complete

---
+++ footer.php
@@ -0,0 +1,23 @@
+<footer class="mt-4">
+    <div class="row container ">
+        <div class="col-md-6 text-center">
+            <h5>Admin Panal</h5>
+            <a href="#">Dashboard</a>
+        </div>
+        <div class="col-md-6 text-center">
+            <h5>connect with us</h5>
+            <div>
+                <a href="#">Facebook</a>
+            </div>
+            <div>
+                <a href="#">Instagram</a>
+            </div>
+            <div>
+
+                <a href="#">Twitter</a>
+            </div>
+        </div>
+    </div>
+    <hr>
+    <div class="text-center foot2"> <p>© 1996-2023, Amazon.com, Inc. or its affiliates</p> </div>
+</footer>
