5f5b7945fb445676861626cdff94e14c60f63aed
krishna soni <soni01krishna@gmail.com>
2023-04-03 12:44:56 +0530

    login page frontend complete

---
+++ pages/login/login.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>amazon.in/login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../css/style.css">
+</head>
+
+<body>
+    <div class="text-bg-light box-sh">
+        <div class="container d-flex justify-content-center">
+            <div class="login">
+                <div class="logo-con text-center">
+                    <img src="../../images/logo2.png" alt="" class="logo">
+
+                </div>
+                <div class="form-cont">
+                    <h2>Sign in</h2>
+                    <form action="">
+                        <div class="my-3">
+                            <label for="">Email or mobile phone number</label>
+                            <input type="text" class="form-control">
+                        </div>
+                        <div class="my-3">
+                            <label for="">Password</label>
+                            <input type="password" class="form-control">
+                        </div>
+                        <div class="my-3 btn-full">
+                            <input type="submit" value="Sign in" class="btn btn-warning btn-full">
+                        </div>
+                    </form>
+                </div>
+                <div class="my-3 text-center">
+                    <button id="btn-caa">Create a Amazon account</button>
+                </div>
+            </div>
+        </div>
+    </div>
+ <div class="text-center my-3">
+    <p>© 1996-2023, Amazon.com, Inc. or its affiliates</p>
+ </div>
+        
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
