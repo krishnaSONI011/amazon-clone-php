bb443875f8e948ec32d20df95959d81878f6df5f
krishna soni <soni01krishna@gmail.com>
2023-04-04 10:08:15 +0530

    product page is complete for fashion only

---
+++ allproduct.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <?php include "navbar.php" ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3 my-3 text-center ">
+                <div class="card text-center" style="width: 18rem;">
+                <div class="text-center">
+                    <img src="images/product-img.jpg" class="card-img-top" alt="..."></div>
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                            the card's content.</p>
+                            <span>&#8377;</sapn> <strong>984</strong>
+                    </div>
+                </div>
+
+            </div>
+         <!-- col-md-3-->
+         
+        </div>
+        <!--row -->
+    </div>
+    <!--container -->
+    <?php include "footer.php" ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
