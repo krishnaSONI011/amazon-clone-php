f4adb700583b24152ee24e6cf37c5d9e104ade24
Laxman Murmu <laxmanmurmu8201@gmail.com>
2023-04-12 01:02:49 +0530

    Address Edited And Deleted Completed

---
+++ address_remove.php
@@ -0,0 +1,10 @@
+<?php 
+ 
+ include 'admin_panel/partials/_dbconnect.php';
+ echo $id = $_GET['id'];
+
+ $sql = "DELETE FROM `address` WHERE `id` = $id";
+ $result = mysqli_query($conn, $sql);
+
+ header('location: all_address.php');
+?>
