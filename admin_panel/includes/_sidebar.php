8cd60008729ca5237d5eae083e0bc4613aa68a4b
krishna soni <soni01krishna@gmail.com>
2023-04-15 18:13:46 +0530

    change logo in the amazon clone

--- admin_panel/includes/_sidebar.php
+++ admin_panel/includes/_sidebar.php
@@ -1,10 +1,10 @@
 <!-- Main Sidebar Container -->
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-        <img src="assets/dist/img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+    <a href="admin.php" class="brand-link">
+        <img src="assets/dist/img/logo2.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
             style="opacity: .8">
-        <span class="brand-text font-weight-light">Admin Panel</span>
+        <span class="brand-text font-weight-light"><span style="color:white">Amazon </span><span style="color:orange">Seller</span></span>
     </a>
 
     <!-- Sidebar -->
@@ -41,22 +41,22 @@
                 <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
                 <li class="nav-item menu-open">
-                    <a href="#" class="nav-link active">
+                    <a href="admin.php" class="nav-link active">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
-                            <i class="right fas fa-angle-left"></i>
+                            <!-- <i class="right fas fa-angle-left"></i> -->
                             
                         </p>
                     </a>
-                    <ul class="nav nav-treeview">
+                    <!-- <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="admin.php" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Dashboard</p>
                             </a>
                         </li>
-                    </ul>
+                    </ul> -->
                 </li>
 
 
