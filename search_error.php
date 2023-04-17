4f12163699a48dc81351c10d435917958f55913f
vivekkumar9955 <126938288+vivekkumar9955@users.noreply.github.com>
2023-04-17 15:13:08 +0530

    search product by product name or category name done

---
+++ search_error.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+  </head>
+  <body>
+    <h2>404</h2>
+  </body>
+</html>
